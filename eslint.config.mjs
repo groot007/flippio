a8531ca55a43e8e3ce8d3f6806a6086614e32233
Mykola Stanislavchuk <kola.stani@gmail.com>
2025-05-31 11:41:10 +0200

    fix(deps): move deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,7 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   ignores: [
-    'example_app/build/**', // For a specific build directory
-    'example_app/android/**', // For a specific Expo build directory
-    'example_app/ios/**', // For a specific Expo build directory
+    'example_app/**', // For a specific build directory
     'resources/**', // For a specific resources directory
   ],
   rules: {
